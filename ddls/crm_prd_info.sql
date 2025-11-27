26a5ab6f29a6bac65dd0aa9b2a76bbc5163510f8
gauthamtk1301 <130126504+gauthamtk1301@users.noreply.github.com>
2025-11-27 15:57:55 +0530

    Create crm_prd_info.sql

---
+++ ddls/crm_prd_info.sql
@@ -0,0 +1,14 @@
+CREATE TABLE gautham.gtk_scm.crm_prd_info (
+  prd_id INT,
+  prd_key STRING,
+  prd_nm STRING,
+  prd_cost INT,
+  prd_line STRING,
+  prd_start_dt DATE,
+  prd_end_dt DATE,
+  cat_id STRING,
+  dwh_create_date TIMESTAMP,
+  audit_checksum STRING,
+  is_active string,
+  effective_start_date timestamp,
+  effective_end_date timestamp)
