31de0369913c37d6b284374f72356c5cabefec89
gauthamtk1301 <130126504+gauthamtk1301@users.noreply.github.com>
2025-11-27 16:36:32 +0530

    Add new columns to erp_loc_a101 table

---
+++ ddls/erp_loc_a101.sql
@@ -0,0 +1,8 @@
+CREATE TABLE gautham.gtk_scm.erp_loc_a101 (
+  cid STRING,
+  cntry STRING,
+  dwh_create_date TIMESTAMP,
+   audit_checksum STRING,
+  is_active string,
+  effective_start_date timestamp,
+  effective_end_date timestamp)
