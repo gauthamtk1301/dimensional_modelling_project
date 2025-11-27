58abfeac523faa7214495af01d9e757295caa7c7
gauthamtk1301 <130126504+gauthamtk1301@users.noreply.github.com>
2025-11-27 16:27:34 +0530

    Create erp_cust_az12.sql

---
+++ ddls/erp_cust_az12.sql
@@ -0,0 +1,9 @@
+CREATE TABLE gautham.gtk_scm.erp_cust_az12 (
+  cid STRING,
+  bdate DATE,
+  gen STRING,
+  dwh_create_date TIMESTAMP,
+ audit_checksum STRING,
+  is_active string,
+  effective_start_date timestamp,
+  effective_end_date timestamp)
