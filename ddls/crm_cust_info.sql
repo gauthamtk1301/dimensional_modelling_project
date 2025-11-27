c15fc3f3a2aa4f413c5df17ce1f7a218fea376d8
gauthamtk1301 <130126504+gauthamtk1301@users.noreply.github.com>
2025-11-27 15:34:12 +0530

    Create crm_cust_info table for customer data

---
+++ ddls/crm_cust_info.sql
@@ -0,0 +1,13 @@
+CREATE TABLE gautham.gtk_scm.crm_cust_info (
+  cst_id INT,
+  cst_key STRING,
+  cst_firstname STRING,
+  cst_lastname STRING,
+  cst_marital_status STRING,
+  cst_gndr STRING,
+  cst_create_date DATE,
+  dwh_create_date TIMESTAMP,
+  audit_checksum STRING,
+  active_flag string,
+  eff_from_date timestamp,
+  eff_thru_date timestamp)
