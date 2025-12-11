eae5d48d15d2976148a52c1a1e20b8cb6c64190d
gauthamtk1301 <130126504+gauthamtk1301@users.noreply.github.com>
2025-12-11 11:45:16 +0530

    Delete ddls directory

--- ddls/erp_cust_az12.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE gautham.gtk_scm.erp_cust_az12 (
-  cid STRING,
-  bdate DATE,
-  gen STRING,
-  dwh_create_date TIMESTAMP,
- audit_checksum STRING,
-  is_active string,
-  effective_start_date timestamp,
-  effective_end_date timestamp)
